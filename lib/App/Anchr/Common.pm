dfb9862daec94bfaccb61b823eaab2ec4824b106
wang-q <wang-q@outlook.com>
2017-02-28 17:42:54 +0800

    poa_consensus()

--- lib/App/Anchr/Common.pm
+++ lib/App/Anchr/Common.pm
@@ -196,4 +196,47 @@ sub g2gv0 {
     Path::Tiny::path($fn)->spew_raw( $gv->as_png );
 }
 
+sub poa_consensus {
+    my $seq_refs = shift;
+
+    my $aln_prog = "poa";
+
+    # temp in and out
+    my $temp_in  = Path::Tiny->tempfile("seq_in_XXXXXXXX");
+    my $temp_out = Path::Tiny->tempfile("seq_out_XXXXXXXX");
+
+    # msa may change the order of sequences
+    my @indexes = 0 .. scalar( @{$seq_refs} - 1 );
+    {
+        my $fh = $temp_in->openw();
+        for my $i (@indexes) {
+            printf {$fh} ">seq_%d\n", $i;
+            printf {$fh} "%s\n",      $seq_refs->[$i];
+        }
+        close $fh;
+    }
+
+    my @args;
+    {
+        push @args, "-hb";
+        push @args, "-read_fasta " . $temp_in->absolute->stringify;
+        push @args, "-clustal " . $temp_out->absolute->stringify;
+        push @args, File::ShareDir::dist_file( 'App-Anchr', 'poa-blosum80.mat' );
+    }
+
+    my $cmd_line = join " ", ( $aln_prog, @args );
+    my $ok = IPC::Cmd::run( command => $cmd_line );
+
+    if ( !$ok ) {
+        Carp::confess("Calling [$aln_prog] failed\n");
+    }
+
+    my $consensus = join "", grep {/^CONSENS0/} $temp_out->lines( { chomp => 1, } );
+    $consensus =~ s/CONSENS0//g;
+    $consensus =~ s/\s//g;
+    $consensus =~ s/-//g;
+
+    return $consensus;
+}
+
 1;
---
+++ t/90-common.t
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Test::More;
+use App::Anchr::Common;
+
+{
+    print "#poa_consensus\n";
+
+    my @data = (
+
+        #                   *
+        [   [   qw{ AAAATTTTGG
+                    AAAATTTTTG }
+            ],
+            "AAAATTTTGG",
+        ],
+
+        #                      *
+        [   [   qw{ TTAGCCGCTGAGAAGC
+                    TTAGCCGCTGA-AAGC }
+            ],
+            "TTAGCCGCTGAGAAGC",
+        ],
+
+        #                   *
+        [   [   qw{ AAAATTTTGG
+                    AAAATTTTTG
+                    AAAATTTTTG }
+            ],
+            "AAAATTTTTG",
+        ],
+    );
+
+    for my $i ( 0 .. $#data ) {
+        my ( $seq_refs, $expect ) = @{ $data[$i] };
+
+        my $result = App::Anchr::Common::poa_consensus($seq_refs);
+        ok( $expect eq $result, "poa $i" );
+    }
+}
+
+done_testing();
