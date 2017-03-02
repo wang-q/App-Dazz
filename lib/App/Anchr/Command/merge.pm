016d62ad30ed7353ade04f1cb0886d510187bfc6
wang-q <wang-q@outlook.com>
2017-03-02 20:19:43 +0800

    `anchr merge`

---
+++ lib/App/Anchr/Command/merge.pm
@@ -0,0 +1,167 @@
+package App::Anchr::Command::merge;
+use strict;
+use warnings;
+use autodie;
+
+use App::Anchr - command;
+use App::Anchr::Common;
+
+use constant abstract => "merge super-reads, k-unitigs, or anchors";
+
+sub opt_spec {
+    return (
+        [ "outfile|o=s", "output filename, [stdout] for screen", ],
+        [ "len|l=i",      "minimal length of overlaps",   { default => 500 }, ],
+        [ "idt|i=f",      "minimal identity of overlaps", { default => 0.98 }, ],
+        [ "prefix=s",     "prefix of names",              { default => "infile" }, ],
+        [ "parallel|p=i", "number of threads",            { default => 8 }, ],
+        [ "verbose|v",    "verbose mode", ],
+        { show_defaults => 1, }
+    );
+}
+
+sub usage_desc {
+    return "anchr merge [options] <infile> [more infiles";
+}
+
+sub description {
+    my $desc;
+    $desc .= ucfirst(abstract) . ".\n";
+    $desc .= "\tAll operations are running in a tempdir and no intermediate files are kept.\n";
+    return $desc;
+}
+
+sub validate_args {
+    my ( $self, $opt, $args ) = @_;
+
+    if ( @{$args} < 1 ) {
+        my $message = "This command need one or more input files.\n\tIt found";
+        $message .= sprintf " [%s]", $_ for @{$args};
+        $message .= ".\n";
+        $self->usage_error($message);
+    }
+    for ( @{$args} ) {
+        if ( !Path::Tiny::path($_)->is_file ) {
+            $self->usage_error("The input file [$_] doesn't exist.");
+        }
+    }
+
+    if ( !exists $opt->{outfile} ) {
+        $opt->{outfile} = Path::Tiny::path( $args->[0] )->absolute . ".merge.fasta";
+    }
+}
+
+sub execute {
+    my ( $self, $opt, $args ) = @_;
+
+    # absolute pathes as we will chdir to tempdir later
+    my @infiles;
+    for my $infile ( @{$args} ) {
+        push @infiles, Path::Tiny::path($infile)->absolute->stringify;
+    }
+
+    if ( lc $opt->{outfile} ne "stdout" ) {
+        $opt->{outfile} = Path::Tiny::path( $opt->{outfile} )->absolute->stringify;
+    }
+
+    # record cwd, we'll return there
+    my $cwd     = Path::Tiny->cwd;
+    my $tempdir = Path::Tiny->tempdir("anchr_merge_XXXXXXXX");
+    chdir $tempdir;
+
+    my $basename = $tempdir->basename();
+    $basename =~ s/\W+/_/g;
+
+    {    # filter short contigs then rename reads as there're duplicated names
+        for my $i ( 0 .. $#infiles ) {
+            my $cmd;
+            $cmd .= "faops filter -a $opt->{len} -l 0 $infiles[$i] stdout";
+            $cmd .= " | anchr dazzname --prefix $opt->{prefix}_$i stdin -o infile.$i.fasta";
+
+            App::Anchr::Common::exec_cmd( $cmd, { verbose => $opt->{verbose}, }, );
+        }
+    }
+
+    {    # overlaps
+        my $cmd;
+        $cmd .= "anchr overlap";
+        $cmd .= " --len $opt->{len} --idt $opt->{idt} --parallel $opt->{parallel}";
+        $cmd .= sprintf " infile.%d.fasta", $_ for ( 0 .. $#infiles );
+        $cmd .= " -o merge.ovlp.tsv";
+        App::Anchr::Common::exec_cmd( $cmd, { verbose => $opt->{verbose}, } );
+
+        if ( !$tempdir->child("merge.ovlp.tsv")->is_file ) {
+            Carp::croak "Failed: create merge.ovlp.tsv\n";
+        }
+    }
+
+    {    # discard some SR
+        my @discards;
+        for my $line ( $tempdir->child("merge.ovlp.tsv")->lines( { chomp => 1, } ) ) {
+            my $info = App::Anchr::Common::parse_ovlp_line($line);
+
+            # ignore self overlapping
+            next if $info->{f_id} eq $info->{g_id};
+
+            # ignore poor overlaps
+            next if $info->{ovlp_idt} < $opt->{idt};
+            next if $info->{ovlp_len} < $opt->{len};
+
+            # discard contained SR
+            if ( $info->{contained} eq q{contains} ) {
+                push @discards, $info->{g_id};
+                next;
+            }
+            if ( $info->{contained} eq q{contained} ) {
+                push @discards, $info->{f_id};
+                next;
+            }
+
+            # discard nearly contained SR
+            my $f_r = $info->{ovlp_len} / $info->{f_len};
+            my $g_r = $info->{ovlp_len} / $info->{g_len};
+
+            if ( $f_r <= 0.9 and $g_r > 0.9 ) {
+                push @discards, $info->{g_id};
+                next;
+            }
+
+            if ( $g_r <= 0.9 and $f_r > 0.9 ) {
+                push @discards, $info->{f_id};
+                next;
+            }
+
+            if ( $f_r > 0.9 and $g_r > 0.9 ) {
+                if ( $info->{f_len} >= $info->{g_len} ) {
+                    push @discards, $info->{g_id};
+                }
+                else {
+                    push @discards, $info->{f_id};
+                }
+                next;
+            }
+
+            # more strict identity
+            next if $info->{ovlp_idt} < 0.99;
+
+            if ( $info->{f_len} / $info->{g_len} > 2 and $g_r > 0.5 ) {
+                push @discards, $info->{g_id};
+                next;
+            }
+        }
+
+        @discards = App::Fasops::Common::uniq( sort @discards );
+        $tempdir->child("discard.txt")->spew( map {"$_\n"} @discards );
+    }
+
+    {    # Outputs. stdout is handeld by faops
+        my $cmd = "cat";
+        $cmd .= sprintf " infile.%d.fasta", $_ for ( 0 .. $#infiles );
+        $cmd .= " | faops some -i -l 0 stdin discard.txt $opt->{outfile}";
+        App::Anchr::Common::exec_cmd( $cmd, { verbose => $opt->{verbose}, } );
+    }
+
+    chdir $cwd;
+}
+
+1;
