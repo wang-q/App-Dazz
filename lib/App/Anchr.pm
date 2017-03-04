b45862b8f3eb00bb60672824542ff27e1615a020
wang-q <wang-q@outlook.com>
2017-03-04 15:00:15 +0800

    Misc changes

--- lib/App/Anchr.pm
+++ lib/App/Anchr.pm
@@ -49,7 +49,7 @@ App::Anchr is tend to be an Assembler of N-free CHRomosomes.
     cpanm --installdeps https://github.com/wang-q/App-Anchr.git
     curl -fsSL https://raw.githubusercontent.com/wang-q/App-Anchr/master/share/install_dep.sh | bash
     curl -fsSL https://raw.githubusercontent.com/wang-q/App-Anchr/master/share/check_dep.sh | bash
-    cpanm -nq https://github.com/wang-q/App-Anchr/archive/0.0.18.tar.gz
+    cpanm -nq https://github.com/wang-q/App-Anchr/archive/0.0.19.tar.gz
     # cpanm -nq https://github.com/wang-q/App-Anchr.git
 
 =head1 AUTHOR
